72d3040219860d15c2b9d218f3eb4d59e8c40a09
Ritesh Prasad <152852634+Ritesh-Prasad@users.noreply.github.com>
2026-03-26 12:12:46 +0530

    Update kubectl_kops.sh

--- kubectl_kops.sh
+++ kubectl_kops.sh
@@ -13,4 +13,4 @@ export KOPS_STATE_STORE=s3://ritesh.k8s.local
 
 kops create cluster --name ritesh.k8s.local --zones ap-south-1a --control-plane-image ami-05d2d839d4f73aafb  --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-05d2d839d4f73aafb  --node-count=2 --node-size c7i-flex.large
 
-TO DELETE: kops delete cluster --name rahams.k8s.local --yes
+TO DELETE: kops delete cluster --name ritesh.k8s.local --yes
